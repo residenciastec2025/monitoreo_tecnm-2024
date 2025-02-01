208584830671f6bb7af5425a576011845e6a302e
residencias <residencias@gmail.com>
2025-02-01 17:18:27 -0600

    another change in cors

--- src/index.mjs
+++ src/index.mjs
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 const allowedOrigins = [
     'http://localhost:4200',
     'https://itc-calificraciones.vercel.app',
-    'adorable-gumdrop-f8e115.netlify.app'
+    'https://monitoreotecnm.netlify.app'
 ];
 
 //Json
