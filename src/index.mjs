5bbe81cc1d84593ac1c8a1e707641280682899bb
residencias <sistemawebmonitoreotec@gmail.com>
2026-01-27 19:19:46 -0600

    Files
    Url host frontend

--- src/index.mjs
+++ src/index.mjs
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const allowedOrigins = [
-    'https://itc-web.vercel.app/',
+    'http://localhost:4200',
     // 'https://itc-calificraciones.vercel.app',
     //'https://monitoreocuautlatecnm.netlify.app'
 ];
